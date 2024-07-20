8fd35dc5326c3c964a0346a9fc3399c38c35a44e
Gordon G <whataever0317@gmail.com>
2024-07-19 22:49:17 -0400

    Update assignment.js

--- 3-javascript/assignment.js
+++ 3-javascript/assignment.js
@@ -71,9 +71,33 @@ const alphabeticalOrder = (str) => {
   return [...str].sort().join("");
 };
 
+String.prototype.alphabeticalOrder = function () {
+  const buff = Array(57).fill(0);
+  const A = "A".charCodeAt(0);
+  for (const c of this) {
+    const p = c.charCodeAt(0) - A;
+    buff[p] += 1;
+  }
+  let s = "";
+  for (let i = 0; i < buff.length; i++) {
+    if (buff[i] > 0) {
+      let count = buff[i];
+      while (count > 0) {
+        s += String.fromCharCode(i + A);
+        count--;
+      }
+    }
+  }
+  return s;
+};
+
 console.log(
   "4. Test alphabeticalOrder: ",
   alphabeticalOrder("webmaster") === "abeemrstw"
+);
+console.log(
+  "4. Test alphabeticalOrder: ",
+  "webmaster".alphabeticalOrder() === "abeemrstw"
 );
 
 /*
@@ -100,25 +124,15 @@ within the string.
 Example string: 'Web Development Tutorial' 
 Expected Output: 'Development'
 */
-const findLongestWord1 = (str) => {
-  let longest = "";
-  for (const w of str.split(" ")) {
-    if (w.length > longest.length) longest = w;
-  }
-  return longest;
-};
-const findLongestWord2 = (str) => {
+const findLongestWord = (str) => {
   return str.split(" ").reduce((longest, w) => {
     return w.length > longest.length ? w : longest;
   }, "");
 };
+
 console.log(
-  "6. Test findLongestWord1: ",
-  findLongestWord1("Web Development Tutorial") === "Development"
-);
-console.log(
-  "6. Test findLongestWord2: ",
-  findLongestWord2("Web Development Tutorial") === "Development"
+  "6. Test findLongestWord: ",
+  findLongestWord("Web Development Tutorial") === "Development"
 );
 
 /*
@@ -146,18 +160,18 @@ prime or not.
 Note: A prime number (or a prime) is a natural number greater than 1 that has no positive
 divisors other than 1 and itself.
 */
-const isPrimeNumber = (num) => {
-  if (num <= 1) return false;
-  if (num === 2) return true;
+Number.prototype.isPrime = function () {
+  if (this <= 1) return false;
+  if (this === 2) return true;
 
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
+  for (let i = 2; i < this; i++) {
+    if (this % i === 0) {
       return false;
     }
   }
   return true;
 };
-console.log("8. Test isPrimeNumber: ", isPrimeNumber(23) === true);
+console.log("8. Test isPrimeNumber: ", Number(23).isPrime() === true);
 
 /*
 9. Write a JavaScript function which accepts an argument and returns the type. 
@@ -349,6 +363,7 @@ const binarySearch = (nums, target) => {
   }
   return left;
 };
+
 console.log(
   "18. Test binarySearch: ",
   binarySearch([1, 2, 3, 4, 6, 8, 12, 16, 24, 48], 3) === 2
@@ -368,3 +383,257 @@ console.log(
     [12, 16, 48]
   )
 );
+
+/*
+20. Write a JavaScript function that generates a string id (specified length) of random characters.
+Sample   character   list:
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+*/
+const randomID = (sample, length) => {
+  let str = "";
+  for (let i = 0; i < length; i++) {
+    str += sample[Math.floor(Math.random() * length)];
+  }
+  return str;
+};
+
+const id = randomID(
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+  6
+);
+console.log("20. Test randomID: ", id, id.length === 6);
+
+/*
+21. Write a JavaScript function to get all possible subset with a fixed length (for example 2)
+combinations in an array. 
+Sample array: [1, 2, 3] and subset length is 2 
+Expected output: [[2, 1], [3, 1], [3, 2]]
+*/
+const allSubsets = (nums, k) => {
+  const l = nums.length;
+  const result = [];
+
+  const recurExplore = (nums, k, idx, comb) => {
+    if (idx > l || k === 0) return [comb];
+
+    const r = [];
+    for (let i = idx; i < l; i++) {
+      r.push(...recurExplore(nums, k - 1, i + 1, [nums[i], ...comb]));
+    }
+    return r;
+  };
+
+  for (let i = 0; i < l; i++) {
+    result.push(...recurExplore(nums, k - 1, i + 1, [nums[i]]));
+  }
+
+  return result;
+};
+
+console.log(
+  "21. Test allSubsets: ",
+  isArrayEqualJSON(allSubsets([1, 2, 3], 2), [
+    [2, 1],
+    [3, 1],
+    [3, 2],
+  ])
+);
+
+/*
+22. Write a JavaScript function that accepts two arguments, a string and a letter and the function
+will count the number of occurrences of the specified letter within the string. 
+Sample arguments: 'microsoft.com', 'o'
+*/
+String.prototype.findOccurrence = function (c) {
+  let count = 0;
+  for (const char of this) {
+    if (char === c) count++;
+  }
+  return count;
+};
+
+console.log(
+  "22. Test findOccurrence: ",
+  "microsoft.com".findOccurrence("o") === 3
+);
+
+/*
+23. Write a JavaScript function to find the first not repeated character. 
+Sample arguments: 'abacddbec' 
+Expected output: 'e'
+*/
+const firstNotRepeated = (str) => {
+  // freqOfString is from question 17
+  const hash = freqOfString(str);
+  for (const c of str) {
+    if (hash.get(c) === 1) return c;
+  }
+};
+
+console.log(
+  "23. Test firstNotRepeated: ",
+  firstNotRepeated("abacddbec") === "e"
+);
+
+/*
+24. Write a JavaScript function to apply Bubble Sort algorithm. 
+Note: According to wikipedia "Bubble sort, sometimes referred to as sinking sort, is a simple
+sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing
+each pair of adjacent items and swapping them if they are in the wrong order". 
+Sample array: [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]
+Expected output: [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
+*/
+const bubbleSort = (nums) => {
+  for (let i = 0; i < nums.length; i++) {
+    let noSwap = true;
+    for (let j = 0; j < nums.length - i; j++) {
+      if (nums[j] < nums[j + 1]) {
+        const temp = nums[j + 1];
+        nums[j + 1] = nums[j];
+        nums[j] = temp;
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return nums;
+};
+
+console.log(
+  "24. Test bubbleSort: ",
+  isArrayEqual(
+    bubbleSort([
+      12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213,
+    ]),
+    [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
+  )
+);
+/*
+25. Write a JavaScript function that accept a list of country names as input and returns the
+longest country name as output. 
+Sample function: Longest_Country_Name(["Australia", "Germany", "United States of America"])
+Expected output: "United States of America"
+*/
+const findLongestCountryName = (arr) => {
+  return arr.reduce((longest, w) => {
+    return w.length > longest.length ? w : longest;
+  }, "");
+};
+
+console.log(
+  "25. Test findLongestCountryName: ",
+  findLongestCountryName([
+    "Australia",
+    "Germany",
+    "United States of America",
+  ]) === "United States of America"
+);
+
+/*
+26. Write a JavaScript function to find longest substring in a given a string without repeating
+characters.
+*/
+const longestSubString = (str) => {
+  const set = new Set();
+  let shrink = 0;
+  let longest = [0, 0];
+
+  for (let i = 0; i < str.length; i++) {
+    const c = str[i];
+    if (!set.has(c)) {
+      set.add(c);
+      if (i - shrink + 1 >= longest[1] - longest[0]) {
+        longest = [shrink, i + 1];
+      }
+    } else {
+      while (set.has(c)) {
+        set.delete(str[shrink]);
+        shrink++;
+      }
+    }
+  }
+  return str.slice(longest[0], longest[1]);
+};
+
+console.log(
+  "26. Test longestSubString: ",
+  longestSubString("occurrence") === "renc"
+);
+/*
+27. Write a JavaScript function that returns the longest palindrome in a given string.
+Note: According to Wikipedia "In computer science, the longest palindromic substring or longest
+symmetric factor problem is the problem of finding a maximum-length contiguous substring of a
+given string that is also a palindrome. For example, the longest palindromic substring of
+"bananas" is "anana". The longest palindromic substring is not guaranteed to be unique; for
+example, in the string "abracadabra", there is no palindromic substring with length greater than
+three, but there are two palindromic substrings with length three, namely, "aca" and "ada".
+In some applications it may be necessary to return all maximal palindromic substrings (that is, all
+substrings that are themselves palindromes and cannot be extended to larger palindromic
+substrings) rather than returning only one substring or returning the maximum length of a
+palindromic substring.
+*/
+const longestPalindrome = (str) => {
+  let result = [];
+  let longest = 0;
+  for (let i = 1; i < str.length - 1; i++) {
+    const palindrome1 = explore(str, i - 1, i + 1);
+    const palindrome2 = explore(str, i, i + 1);
+
+    if (palindrome1.length > longest) {
+      result = [palindrome1];
+      longest = palindrome1.length;
+    } else if (palindrome1.length === longest) {
+      result.push(palindrome1);
+    }
+
+    if (palindrome2.length > longest) {
+      result = [palindrome2];
+      longest = palindrome2.length;
+    } else if (palindrome2.length === longest) {
+      result.push(palindrome2);
+    }
+  }
+
+  return result;
+};
+const explore = (str, idx1, idx2) => {
+  while (idx1 >= 0 && idx2 < str.length && str[idx1] === str[idx2]) {
+    idx1--;
+    idx2++;
+  }
+  return str.slice(idx1 + 1, idx2);
+};
+
+console.log(
+  "27. Test longestPalindrome: ",
+  isArrayEqual(longestPalindrome("abracadabra"), ["aca", "ada"])
+);
+
+/*
+28. Write a JavaScript program to pass a 'JavaScript function' as parameter.
+*/
+Array.prototype.myReduce = function (cb, initialVal) {
+  if (initialVal === undefined || initialVal === null) {
+    throw new EvalError("initialVal is undefined or null");
+  }
+  let accumulator = initialVal;
+  for (const e of this) {
+    accumulator = cb(accumulator, e);
+  }
+  return accumulator;
+};
+const sum = [1, 2, 3].myReduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+console.log("28. Test myReduce: ", sum === 6);
+
+/*
+29. Write a JavaScript function to get the function name.
+*/
+const myFunction = () => {};
+console.log("28. Test myFunction: ", myFunction.name === "myFunction");
+function getFuncName() {
+  console.log(getFuncName.arguments.callee.name);
+  console.log(getFuncName.name);
+}
+getFuncName();
